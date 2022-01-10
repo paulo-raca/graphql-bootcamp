87b1c96973561ee9a26740b997daf8bb03030673
Paulo Costa <me@paulo.costa.nom.br>
2022-01-10 16:47:54 -0300

    2.8 - GraphQL Scalar types

--- src/index.js
+++ src/index.js
@@ -1,36 +1,38 @@
 import { GraphQLServer } from "graphql-yoga";
 
 // Type Definitions (schema)
-
 const typeDefs = `
-    type Person {
-      name: String!
-      bio: String!
-      location: String!
-    }
     type Query {
-        hello: String!
+        id: ID!
         name: String!
-        me: Person
+        age: Int!
+        employed: Boolean!
+        gpa: Float
+
+
+        title: String!
+        price: Float!
+        releaseYear: Int
+        rating: Float
+        inStock: Boolean!
     }
 `
 
 // Resolver
 const resolvers = {
   Query: {
-    hello() {
-      return 'Hi!'
-    },
-    name() {
-      return 'Paulo Costa'
-    },
-    me() {
-      return {
-        name: 'Paulo Costa',
-        bio: 'Sou legal',
-        location: 'Campinas, SP, Brazil'
-      }
-    }
+    id: () => '5656',
+    name: () => 'Paulo Costa',
+    age: () => 34,
+    employed: () => false,
+    gpa: () => null,
+
+
+    title: () => "Thingy",
+    price: () => 99.99,
+    releaseYear: () => 1998,
+    rating: () => 4.7,
+    inStock: () => true,
   }
 }
 
