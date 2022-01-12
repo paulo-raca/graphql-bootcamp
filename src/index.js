15af394b750121d3ed16c22aa06277895263b635
Paulo Costa <me@paulo.costa.nom.br>
2022-01-12 12:08:14 -0300

    2.13 - Working with Arrays Part 2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,54 @@
 import { GraphQLServer } from "graphql-yoga";
 
+const users = [
+  {
+    id: '5656',
+    name: 'Paulo Costa',
+    email: 'paulo@example.com',
+    age: 34,
+  }, 
+  {
+    id: '17',
+    name: 'Zé',
+    email: 'joseph@example.com',
+    age: null,
+  }, 
+  {
+    id: '34',
+    name: 'Beatriz',
+    email: 'bia@example.com',
+    age: 30,
+  },
+]
+
+const posts = [
+  {
+    id: '1',
+    title: 'Everything is Awesome',
+    body: "Haven't you watched the Lego Movie yet?",
+    published: true
+  },
+  {
+    id: '2',
+    title: 'Mary had a little lamb',
+    body: "It's fleece was white as snow\nAnd everythere that Mary went,\nThe lamb was sure to go",
+    published: true
+  },
+  {
+    id: '3',
+    title: 'Let\'s learn GraphQL',
+    body: "Work in progress...",
+    published: false
+  },
+]
+
 // Type Definitions (schema)
 const typeDefs = `
     type Query {
       me: User!
       post: Post!
-      add(numbers: [Float!]!): Float!
-      greet(name: String!): String!
-      grades: [Int!]!
+      users(query: String): [User!]!
+      posts(query: String, published: Boolean): [Post!]!
     }
 
     type User {
@@ -29,30 +70,31 @@ const typeDefs = `
 const resolvers = {
   Query: {
     me: () => {
-      return {
-        id: '5656',
-        name: 'Paulo Costa',
-        email: 'paulo@example.com',
-        age: 34,
-      }
+      return users[0]
     },
     post: () => {
-      return {
-        id: '1234',
-        Title: 'Everything is Awesome',
-        body: "Haven't you watched the Lego Movie yet?",
-        published: true
+      return posts[0]
+    },
+    users: (root, args, context) => {
+      var ret = users
+      if (args.query) {
+        ret = ret.filter((user) => user.name.toLowerCase().includes(args.query.toLowerCase()))
       }
+      return ret
     },
-    greet: (root, args, context) => {
-      return `Hello, ${args.name}`
+    posts: (root, args, context) => {
+      var ret = posts
+      if (args.query) {
+        ret = ret.filter((post) => {
+          return post.title.toLowerCase().includes(args.query.toLowerCase())
+              || post.body.toLowerCase().includes(args.query.toLowerCase())
+        })
+      }
+      if (args.published != null) {
+        ret = ret.filter((post) => post.published == args.published)
+      }
+      return ret
     },
-    add: (root, args, context) => {
-      return args.numbers.reduce( (a, b) => a + b, 0 )
-    },
-    grades: () => {
-      return [1, 2, 3, 4]
-    }
   }
 }
 
